a7be8948f238c852c0a67161b72bd1727a44d712
luizbaidum <luiz.baidum@hotmail.com.br>
2022-07-20 19:59:53 -0300

    ajustes: sair.php e acesso.php
     - session_destroy funcionando
     - proibido acesso a home p/ nao logados

--- scripts/acesso.php
+++ scripts/acesso.php
@@ -1,8 +1,12 @@
-<?php 
+<?php
 
 session_start();
 
-$nome = $_SESSION['login'];
-$acesso = $_SESSION['acesso'];
+if(!isset($_SESSION) || $_SESSION['logado'] != 1) {
 
-
+    header('Location: index.php');
+} else {
+    
+    $nome = $_SESSION['login'];
+    $acesso = $_SESSION['acesso'];
+}
