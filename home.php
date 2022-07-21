6adff51453f5611a3ac28d15f5aa281998b8a87d
luizbaidum <luiz.baidum@hotmail.com.br>
2022-07-20 21:17:01 -0300

    miolo da pagina home pronto!

--- home.php
+++ home.php
@@ -8,19 +8,10 @@
 
         <?php require_once 'barrinha.php'; ?>
 
-        <main class="container">
-
-            <div>Bem-vindo(a)!</div>
-            <div id="menu">
-                <a id="usuarios">Usuários</a>
-                <span>Se F = só pode atualizar a si mesmo / Se A = vê lista de usuários, pode criar novo usuário, pode apagar usuário e atualizar a si mesmo</span>
-
-                <a id="cad-produtos">Cadastro de Produtos</a>
-                <span>F e A fazem a mesma coisa = cadastram produtos</span>
-
-                <a id="produtos">Lista de Produtos Cadastrados</a>
-                <span>F só visualiza produtos cadastrados / Se a visualiza produtos e pode apagar e deletar</span>
-            </div>
-        </main>
+        <?php if ($acesso == 0)
+                require 'paginas/main-home-f.php';
+              else
+                require 'paginas/main-home-a.php';
+        ?>
     </body>
 </html>
---
+++ paginas/main-home-f.php
@@ -0,0 +1,1 @@
+COLAR DE A
