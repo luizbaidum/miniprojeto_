f30f5956e60e10f320d6bfb4a6473bdde97a0bae
Luiz Baidum <89556382+luizbaidum@users.noreply.github.com>
2022-09-23 20:29:57 -0300

    removidos dados sensíveis

--- server/conectar.php
+++ server/conectar.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $con = new PDO("mysql:host=localhost;dbname=miniprojeto_conag;charset=utf8","root","");
+    $con = new PDO("mysql:host= * seu host *;dbname= * seu db *;charset=utf8","*seu usuario*","*sua senha*");
 
 } catch (Exception $e) {
     throw new Exception('Erro durante a conexão');
